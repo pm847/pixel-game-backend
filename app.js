e480d78d83f768eb81a265ebfb213c2649b4c669
Jason Huang <jasson15@gmail.com>
2016-03-19 07:27:29 +0000

    No cache

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.disable('etag');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
