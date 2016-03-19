bcaf64ebd08e70071f4bcc7015220ec6c5fcbd1d
Jason Huang <jasson15@gmail.com>
2016-03-19 06:07:52 +0000

    Create sample JSON reponse

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var board = require('../lib/board');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// get board
+router.get('/board', (req, res) => {
+    res.json(board.getBoard());
 });
 
 module.exports = router;
